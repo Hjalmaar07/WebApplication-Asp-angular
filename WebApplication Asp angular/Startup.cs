f4fe05a313ee36f05eb40b692890c93b5c7ed7b2
Damian Kuberski <damiank9447@gmail.com>
2021-01-24 17:14:43 +0100

    Merge

--- WebApplication Asp angular/Startup.cs
+++ WebApplication Asp angular/Startup.cs
@@ -31,6 +31,7 @@ namespace WebApplication_Asp_angular
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
